0ed0f13da548ed9d16932bede0022b1251f43861
jittimat <jittimat.r@gmail.com>
2015-12-22 10:26:37 +0700

    buttonController done.

--- app/src/main/java/com/example/jittimat_r/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/jittimat_r/mytraffic/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.jittimat_r.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+    // explicit = define  variable
+    private ListView trafficListView;
+    private Button aboutMeButton;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+       } // Main Method
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.sheep);
+                buttonMediaPlayer.start();
+
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://www.youtube.com/watch?v=92a7Hj0ijLs"));
+                startActivity(objIntent);
+            } //event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);  //หารายชื่อบน file .java
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    }
+
+      } // Main Class
+
+
+
